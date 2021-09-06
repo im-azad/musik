48147cfb0adc84b2eb11cb74222ebf22754ce384
Abul Kalam Azad <me.azad99@gmail.com>
2021-09-06 21:51:57 +0600

    Some changes

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 		slidesToShow: 1,
 		slidesToScroll: 1,
 		autoplay: true,
-		autoplaySpeed: 3000,
+		autoplaySpeed: 60000,
 	});
 
 	$('.blog-slider').slick({
@@ -12,5 +12,30 @@ $(document).ready(function () {
 		autoplay: true,
 		autoplaySpeed: 4000,
 		arrows: false,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+					infinite: true,
+					dots: true,
+				},
+			},
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+				},
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				},
+			},
+		],
 	});
 });
